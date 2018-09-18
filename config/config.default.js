d12b3694687e7c02aa2b685b6de68815aacf15ae
俞学文 <8586826@qq.com>
2018-09-18 10:44:04 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-aliyun-api-gateway default config
+ * @member Config#aliyunApiGateway
+ * @property {String} SOME_KEY - some description
+ */
+exports.aliyunApiGateway = {
+
+};
