17cc088bdbb8b4eac35f6c383c61eb11e5da2f02
xiaoemoxiw <8586826@qq.com>
2018-11-28 21:09:14 +0800

    add config

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Client = require('./lib/aliyun-api-gateway/client');
 
 module.exports = app => {
 
-  const { appKey, appSecret } = app.config.aliyunApiGateway || {};
+  const { appKey, appSecret, stage, verbose } = app.config.aliyunApiGateway || {};
 
   // check key & secret
   assert(appKey && appSecret,
@@ -13,6 +13,15 @@ module.exports = app => {
 
   app.coreLogger.info('[egg-egg-aliyun-api-geteway] setup');
 
-  app.aliyunApiGateway = new Client(appKey, appSecret);
+  const option = [];
+  if (stage) {
+    option.push(stage);
+  }
+
+  if (typeof verbose === 'boolean') {
+    option.push(verbose);
+  }
+
+  app.aliyunApiGateway = new Client(appKey, appSecret, ...option);
 
 };
